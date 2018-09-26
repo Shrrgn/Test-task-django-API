1e5da20311b2ccdb05de97f190e891f341de61b3
Shrrgn <mr.shrrgn@gmail.com>
2018-09-27 02:42:10 +0300

    Added index page

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
 # Create your views here.
+
+class IndexView(View):
+
+	template_name = 'mainApp/index.html'
+
+	def get(self, request, *args, **kwargs):
+		context = {'values':'Harry'}
+		return render(self.request, self.template_name, context)
