a407bf30cc90068f54b5d23879802cc59094f45b
Shrrgn <mr.shrrgn@gmail.com>
2018-10-03 17:23:22 +0300

    Added UserAdmin

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from mainApp.models import Post
+
+User = get_user_model()
 
 # Register your models here.
 
+class UserAdmin(admin.ModelAdmin):
+	ordering = ['-date_joined']
+	list_display = ['username', 'date_joined', 'is_staff']
+
 class PostAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug':('title',)}
+	list_display = ('title', 'author')
 
 admin.site.register(Post, PostAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
